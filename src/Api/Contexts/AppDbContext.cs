cde90b78216d61599190423a33b04a8471df3f35
Ivan Camilo Suarez Castro <ivanca.suarez@gmail.com>
2024-09-14 17:20:50 -0700

    tables

--- src/Api/Contexts/AppDbContext.cs
+++ src/Api/Contexts/AppDbContext.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MiniHackaton.Domain.Entities;
 
 namespace MiniHackaton.Api.Contexts
 {
     public class AppDbContext : DbContext
     {
         #region Tables
-
+        public DbSet<LearningPath> LearningPaths { get; set; }
         #endregion
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         { }
-
-
     }
 }
